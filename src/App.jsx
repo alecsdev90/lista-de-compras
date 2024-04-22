5d11e85800fdf170571955af77624ebfef6d2d02
alecsdev90 <alecostasan.dev@gmail.com>
2024-04-22 15:52:12 -0300

    atualização do projeto

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import ShoppingList from '../ShoppingList'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ShoppingList />
+  </React.StrictMode>,
+)
